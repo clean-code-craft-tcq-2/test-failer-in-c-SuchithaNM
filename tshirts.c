cc6a070ebc7ae47172159f268a30c41a5e72e92a
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-01-22 14:05:36 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -17,8 +17,8 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(38) != 'L'); // tshirt size 38 is not belongs to 'S' and 'M'
-    assert(size(42) != 'S'); // tshirt size 42 is not belongs to 'L' and 'M'
+    assert(size(38) != 'L'); // tshirt size 38 is either belongs to 'S' or 'M'
+    assert(size(42) != 'S'); // tshirt size 42 is either belongs to 'L' or 'M'
     assert((size(-10) != 'S') && (size(-10) != 'M') && (size(-10) != 'L')); //tshirts size -10 not a valid size
     assert((size(0) != 'S') && (size(0) != 'M') && (size(0) != 'L')); //tshirts size 0 not a valid size
     assert((size(100) != 'S') && (size(100) != 'M') && (size(100) != 'L')); //tshirts size 100 not a valid size
