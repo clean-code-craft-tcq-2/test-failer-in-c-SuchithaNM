75e220f55747c562861729a082587bb5318c84ec
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-01-24 14:02:08 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -21,10 +21,7 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
-    if((farenheit > 99) && (farenheit < 500))
-        return 1;
-    else
-        return 0;
+
 }
 
 int main() {
