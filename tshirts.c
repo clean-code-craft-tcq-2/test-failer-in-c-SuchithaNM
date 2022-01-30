0de2c5bfa9d41e2cf8e028db7e896e0011c1f664
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-01-30 16:17:56 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include <assert.h>
+#include "tshirts.h"
+
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms < SMALLSIZE) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms >= SMALLSIZE && cms < LARGESIZE) {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if(cms >= LARGESIZE) {
         sizeName = 'L';
     }
     return sizeName;
 }
 
-int main() 
+
+void test_envTshirtSize()
 {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     assert(size(38) != 'L'); // tshirt size 38 is either belongs to 'S' or 'M'
     assert(size(42) != 'S'); // tshirt size 42 is either belongs to 'L' or 'M'
-  /*  assert((size(-10) != 'S') && (size(-10) != 'M') && (size(-10) != 'L')); //tshirts size -10 not a valid size
-    assert((size(0) != 'S') && (size(0) != 'M') && (size(0) != 'L')); //tshirts size 0 not a valid size
-    assert((size(100) != 'S') && (size(100) != 'M') && (size(100) != 'L')); //tshirts size 100 not a valid size*/
+
+}
+
+int main() 
+{
+    test_envTshirtSize();
     printf("All is well (maybe!)\n");
     return 0;
 }
