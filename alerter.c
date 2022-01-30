3060de33014505b297e52ec0c4e86e5eaa04b53e
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-01-30 16:12:59 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
+#include "alerter.h"
 
-#define MAX_CELCIUS 45
+
 int alertFailureCount = 0;
 
 
@@ -16,6 +17,7 @@ int networkAlertStub(float celcius) {
     }
     else
     {
+	printf("ALERT: Temperature is %.1f celcius.\n", celcius);
         return 500;
     }
 }
@@ -26,33 +28,44 @@ float convertingFarenhitToCelcius(float farenheit)
     celcius = (farenheit - 32) * 5 / 9;
     return celcius;
 }
-void alertInCelcius(float farenheit) 
+void alertInCelcius(float farenheit, int (*fpNetworkAlert)(float)) 
 {
     float celcius  = convertingFarenhitToCelcius(farenheit);
-    int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
+    int returnCode = fpNetworkAlert(celcius);
+	printf("returnCode = %d\n", returnCode);
+    if (returnCode != 200) 
+    {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
-    else{
+    else
+    {
         alertFailureCount += 1;
     }
 
 }
 void test_alertInCelcius(float farenheit)
 {
-    alertInCelcius(farenheit);
+    alertInCelcius(farenheit, &networkAlertStub);
+    printf("%d alerts failed.\n", alertFailureCount);
 }
 
-int main() {
+void test_envTempAlert(void)
+{
     test_alertInCelcius(400.5);
-   // assert(alertFailureCount == 1);
+    //assert(alertFailureCount == 1);
     test_alertInCelcius(303.6);
    // assert(alertFailureCount == 2);
-    printf("%d alerts failed.\n", alertFailureCount);
+    test_alertInCelcius(200);
+    test_alertInCelcius(111.2);
+}
+
+int main() 
+{
+    test_envTempAlert();
     printf("All is well (maybe!)\n");
     return 0;
 }
